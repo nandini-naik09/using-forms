217d4b7cd6a0bc2aa95fbdbec0be5cc3e12f9230
nandini-naik09 <nandinipardeshi99@gmail.com>
2023-05-17 12:46:20 +0530

    Using React Forms with events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+constructor(){
+  super();
+  this.state={
+    participate:"",
+    roll_no:null
+};
+}
+
+changeHandler=(event)=>{
+
+  let name=event.target.name;
+  let val= event.target.value;
+
+if(name==="roll_no")
+{
+  if(!Number(val))
+  {
+    alert("Roll numer must be Number.");
+  }
+}
+
+  this.setState({
+    [name]:val
+  });
+}
+
+submitHandler=(event)=>{
+  event.preventDefault();
+  alert(this.state.participate + " Registered")
+}
+
+
+  render(){
+
+let header="";
+if(this.state.participate)
+{
+  header=<h1>Thank you for registration {this.state.participate}</h1>
+}
+
+    return(
+
+      <form onSubmit={this.submitHandler}>
+        {header}
+        <h1>
+          Welcome {this.state.participate}
+        </h1>
+        <p>Register your name:</p>
+
+        <input type='text' 
+        placeholder='Name'
+        name='participate'
+        onChange={this.changeHandler}/>
+
+      <p>Enter your roll_no</p>
+
+        <input type='text' 
+        placeholder='Roll No'
+        name='roll_no'
+        onChange={this.changeHandler}/>
+        <br/> <br/>
+        <input type='submit' />
+
+        <br/> 
+        <h2> Your roll_no is {this.state.roll_no} </h2>
+      </form>
+
+
+    )
+  }
 }
 
 export default App;
